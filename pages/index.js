848fe9137befd409873f43b461aaa7b5e99e5dff
Dibya1507munu <djnwork1507@gmail.com>
2025-06-04 19:07:40 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "25%" }}>
+      <h1>🚀 NOIRALGO DEPLOYED SUCCESSFULLY!</h1>
+      <p>You're now live on Vercel.</p>
+    </div>
+  );
+}
